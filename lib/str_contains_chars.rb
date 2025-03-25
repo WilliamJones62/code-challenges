f2f4022a2c534e71e3732b40ad7b570d18a5e7c9
Bill Jones <williamjones120862@gmail.com>
2025-03-25 08:59:25 -0400

    fixed rubocop too many lines and complexity errors

--- lib/str_contains_chars.rb
+++ lib/str_contains_chars.rb
@@ -4,15 +4,20 @@ def str_contains_chars(arr)
   result = []
   current_char = 0
   (0...arr[0].size).each do |i|
-    (current_char...arr[1].size).each do |j|
-      next unless arr[0][i] == arr[1][j]
-
-      result.push(arr[0][i])
-      current_char = j + 1
-      break
-    end
+    current_char, result = find_char(current_char, arr, i, result)
   end
   test_result(result, arr[1])
+end
+
+def find_char(current_char, arr, int, result)
+  (current_char...arr[1].size).each do |j|
+    next unless arr[0][int] == arr[1][j]
+
+    result.push(arr[0][int])
+    current_char = j + 1
+    break
+  end
+  [current_char, result]
 end
 
 def test_result(result, arr1)
