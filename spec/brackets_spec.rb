5f368ab28320fe253772d55b9740c2539ff2f88a
Bill Jones <williamjones120862@gmail.com>
2025-03-25 07:56:34 -0400

    Fixed rubocop too many lines error

--- spec/brackets_spec.rb
+++ spec/brackets_spec.rb
@@ -5,14 +5,6 @@ require 'brackets'
 # Return the 1 if a string contains matching brackets, else return 0
 describe 'brackets' do
   it 'returns 1 if the brackets match' do
-    expect(brackets('(hello (world))')).to eq(1)
-  end
-
-  it 'returns 0 if the brackets do not match' do
-    expect(brackets('((hello (world))')).to eq(0)
-  end
-
-  it 'returns 1 if the brackets match' do
     expect(brackets('(c(oder)) b(yte)')).to eq(1)
   end
 
