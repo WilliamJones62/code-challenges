46cc5ad1145d2d1a3218e699c7579a32c807c375
Bill Jones <williamjones120862@gmail.com>
2025-04-03 11:13:22 -0400

    moved test for no characters betweeb numbers to validation method

--- lib/swap.rb
+++ lib/swap.rb
@@ -18,7 +18,7 @@ class Swap
   end
 
   def process_between_numbers(index_array, string, swap_array)
-    if (index_array[1] - index_array[0]) < 2 || !valid_chars(string, index_array)
+    if !valid_chars(string, index_array)
       index_array.shift
     else
       index_array, swap_array = swap_numbers(index_array, swap_array)
@@ -35,9 +35,11 @@ class Swap
   end
 
   def valid_chars(string, index_array)
-    between_numbers = string[index_array[0] + 1..index_array[1] - 1]
-    return true if between_numbers.count('a-zA-Z') == between_numbers.length
+    return false if index_array[1] - index_array[0] < 2
 
-    false
+    between_numbers = string[index_array[0] + 1..index_array[1] - 1]
+    return false if between_numbers.count('a-zA-Z') != between_numbers.length
+
+    true
   end
 end
