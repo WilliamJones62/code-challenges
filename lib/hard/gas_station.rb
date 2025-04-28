8be3a73b3c15bfccaa183706d16c4e89fb5ade98
Bill Jones <williamjones120862@gmail.com>
2025-04-28 13:47:16 -0400

    Refactored gas station challenge to use instance variables

--- lib/hard/gas_station.rb
+++ lib/hard/gas_station.rb
@@ -3,46 +3,44 @@
 # This class contains gas station logic
 class GasStation
   def gas_station(array)
-    total_stations, gas_array = create_variables(array)
-    find_starting_station(total_stations, gas_array, 0, 0, 0)
+    create_variables(array)
+    find_starting_station
   end
 
   def create_variables(array)
-    total_stations = array.shift.to_i
-    gas_array = []
+    @total_stations = array.shift.to_i
+    @station_count = @loop_count = @total_gas = 0
+    @gas_array = []
     array.each do |string|
       gas_string_array = string.split(':')
       gas_string_array.map!(&:to_i)
-      gas_array << gas_string_array
+      @gas_array << gas_string_array
     end
-
-    [total_stations, gas_array]
   end
 
-  def find_starting_station(total_stations, gas_array, station_count, loop_count, total_gas)
-    while station_count < total_stations
-      while loop_count < total_stations
-        total_gas += gas_array[loop_count][0]
-        break if total_gas < gas_array[loop_count][1]
+  def find_starting_station
+    while @station_count < @total_stations
+      while @loop_count < @total_stations
+        @total_gas += @gas_array[@loop_count][0]
+        break if @total_gas < @gas_array[@loop_count][1]
 
-        total_gas, loop_count = load_variables_for_next_loop(total_gas, loop_count, gas_array)
+        load_variables_for_next_loop
       end
-      return (station_count + 1).to_s if loop_count == total_stations
+      return (@station_count + 1).to_s if @loop_count == @total_stations
 
-      station_count, gas_array, loop_count, total_gas = load_variables_for_next_station(station_count, gas_array)
+      load_variables_for_next_station
     end
     'impossible'
   end
 
-  def load_variables_for_next_loop(total_gas, loop_count, gas_array)
-    total_gas -= gas_array[loop_count][1]
-    loop_count += 1
-    [total_gas, loop_count]
+  def load_variables_for_next_loop
+    @total_gas -= @gas_array[@loop_count][1]
+    @loop_count += 1
   end
 
-  def load_variables_for_next_station(station_count, gas_array)
-    station_count += 1
-    gas_array.rotate!
-    [station_count, gas_array, 0, 0]
+  def load_variables_for_next_station
+    @station_count += 1
+    @gas_array.rotate!
+    @loop_count = @total_gas = 0
   end
 end
