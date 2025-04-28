519e94b53c21020a643239a2c6cfbd9e7cfce1bc
Bill Jones <williamjones120862@gmail.com>
2025-04-28 12:30:46 -0400

    Refactored array jumping to use instance variables

--- lib/hard/array_jumping.rb
+++ lib/hard/array_jumping.rb
@@ -4,69 +4,64 @@
 class ArrayJumping
   @max_index = 0
   def array_jumping(array)
-    visited, array_length, jump_array, jump_count = initialize_variables(array)
-    process_jumps(visited, array_length, jump_array, jump_count, array)
+    @array = array
+    initialize_variables
+    process_jumps
   end
 
-  def process_jumps(visited, array_length, jump_array, jump_count, array)
-    until jump_array.empty?
-      next_jump_array = []
-      jump_array.each do |j|
-        next_moves = calculate_next_moves(j, array, array_length)
-        back_to_start, visited, next_jump_array = process_next_moves(next_moves, visited, next_jump_array)
-        return jump_count if back_to_start
+  def process_jumps
+    until @jump_array.empty?
+      @next_jump_array = []
+      @jump_array.each do |j|
+        calculate_next_moves(j)
+        return @jump_count if process_next_moves
       end
 
-      jump_count, jump_array = update_variables(jump_count, next_jump_array)
+      update_variables
     end
     -1
   end
 
-  def initialize_variables(array)
-    visited = []
-    array.length.times do
-      visited << false
+  def initialize_variables
+    @visited = []
+    @array.length.times do
+      @visited << false
     end
 
-    array_length = array.length
-    @max_index = array.index(array.max)
-    visited[@max_index] = true
-    jump_array = [array.index(array.max)]
-    jump_count = 1
-
-    [visited, array_length, jump_array, jump_count]
+    @array_length = @array.length
+    @max_index = @array.index(@array.max)
+    @visited[@max_index] = true
+    @jump_array = [@array.index(@array.max)]
+    @jump_count = 1
   end
 
-  def update_variables(jump_count, next_jump_array)
-    jump_count += 1
-    jump_array = next_jump_array
-    [jump_count, jump_array]
+  def update_variables
+    @jump_count += 1
+    @jump_array = @next_jump_array
   end
 
-  def calculate_next_moves(jump_index, array, array_length)
-    next_moves = []
-    jump = array[jump_index]
-    next_moves.push(jump_index - jump)
-    next_moves[0] = array_length + next_moves[0] if next_moves[0].negative?
-    next_moves.push(jump_index + jump)
-    next_moves[1] = next_moves[1] % array_length if next_moves[1] > array_length
-    next_moves
+  def calculate_next_moves(jump_index)
+    @next_moves = []
+    jump = @array[jump_index]
+    @next_moves.push(jump_index - jump)
+    @next_moves[0] = @array_length + @next_moves[0] if @next_moves[0].negative?
+    @next_moves.push(jump_index + jump)
+    @next_moves[1] = @next_moves[1] % @array_length if @next_moves[1] > @array_length
   end
 
-  def process_next_moves(next_moves, visited, next_jump_array)
-    next_moves.each do |n|
+  def process_next_moves
+    @next_moves.each do |n|
       return true if n == @max_index
 
-      visited, next_jump_array = load_next_jump_array(visited, next_jump_array, n)
+      load_next_jump_array(n)
     end
-    [false, visited, next_jump_array]
+    false
   end
 
-  def load_next_jump_array(visited, next_jump_array, next_move)
-    unless visited[next_move]
-      visited[next_move] = true
-      next_jump_array.push(next_move)
-    end
-    [visited, next_jump_array]
+  def load_next_jump_array(next_move)
+    return if @visited[next_move]
+
+    @visited[next_move] = true
+    @next_jump_array.push(next_move)
   end
 end
